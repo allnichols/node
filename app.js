d2c5cb3b5da4823c924325de7cc464513dc22114
allnichols <allenmaxnichols@gmail.com>
2020-02-06 09:38:52 -0600

    added mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
+const mongoose = require('mongoose');
 const app = express();
 
 // app.use(bodyParser.urlencoded()) // x-www-form-urlencoded <form>
@@ -12,5 +13,9 @@ app.use((req, res, next) => {
     next();
 })
 app.use('/feed', feedRoutes);
+mongoose.connect(
+    'mongodb+srv://max:<password>@cluster0-gx8g5.mongodb.net/test?retryWrites=true&w=majority'
+).then(result => {
+    app.listen(8080);
+}).catch(err => console.log(err));
 
-app.listen(8080);
